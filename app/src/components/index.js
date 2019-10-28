0e150e3f6c6e7ec1f41c5eb06c6dbb3e3a89a35e
Raphael <raphael@adekz.com>
2019-10-27 22:00:29 -0300

    router

--- app/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from './header';
-export * from './content';
-export * from './tabs';
-export * from './newPost';
-export * from './miniPhoto';
-export * from './stories';
-export * from './posts';
